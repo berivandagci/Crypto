9918b0fe32096dbf7f6ee1b95e09aac862886b6a
berivan dağcı <100922483+berivan0108@users.noreply.github.com>
2026-08-19 15:07:44 +0300

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by beri on 19.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoTests/CryptoTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTests.swift
+//  CryptoTests
+//
+//  Created by beri on 19.08.2026.
+//
+
+import Testing
+@testable import Crypto
+
+struct CryptoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
