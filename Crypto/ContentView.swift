63013240b35a7621ecaf488096261331f4636c52
berivan dağcı <100922483+berivan0108@users.noreply.github.com>
2026-08-19 15:56:04 +0300

    İlk commit: Proje başlangıcı

--- Crypto/ContentView.swift
+++ Crypto/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color.theme.backgorund
+                .ignoresSafeArea()
+            VStack(spacing: 40){
+                Text("Accent Color")
+                    .foregroundColor(Color.theme.accent)
+                Text("Red Color")
+                    .foregroundColor(Color.theme.red)
+                Text("Secondarytextcolor Color")
+                    .foregroundColor(Color.theme.secondaryText)
+                
+                Text("green Color")
+
+                    .foregroundColor(Color.theme.green)
+            }
         }
-        .padding()
     }
 }
 
---
+++ Crypto/Extensions/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  Crypto
+//
+//  Created by beri on 19.08.2026.
+//
+import SwiftUI
+import Foundation
+
+extension Color{
+    static let theme = ColorTheme()
+}
+struct ColorTheme{
+    let accent = Color("AccentColor")
+    let backgorund = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
