d0f165e2bedc9a0f16d0354f87e6735d39d60959
berivan dağcı <100922483+berivan0108@users.noreply.github.com>
2026-08-28 18:45:50 +0300

    Add CoinDataService and connect API data to HomeViewModel using Combine

--- Crypto/Core/Home/ViewModels/HomeViewModel.swift
+++ Crypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -6,21 +6,25 @@
 //
 
 import Foundation
+import Combine
+
 class HomeViewModel: ObservableObject {
     
-    @Published var allCoins : [CoinModel] = []
-    @Published var portofolioCoins : [CoinModel] = []
+    @Published var allCoins: [CoinModel] = []
+    @Published var portofolioCoins: [CoinModel] = []
     
+    private let dataService = CoinDataService()
+    var cancellables = Set<AnyCancellable>()
     
+    init() {
+        addSubscribers()
+    }
     
-    init(){
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            self.allCoins.append(DeveloperPreview.instance.coin)
-            self.portofolioCoins.append(DeveloperPreview.instance.coin)
-            
-            
-            
-        }
-        
+    func addSubscribers() {
+        dataService.$allCoins
+            .sink { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
     }
 }
