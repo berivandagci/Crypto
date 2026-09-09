4cf7c42f9ffe89e84e6e822a74129584c27d5d9e
berivan dağcı <100922483+berivan0108@users.noreply.github.com>
2026-09-09 18:38:08 +0300

    Update CryptoApp with launch screen flow and configuration

--- Crypto/Core/Home/Views/HomeView.swift
+++ Crypto/Core/Home/Views/HomeView.swift
@@ -1,59 +1,74 @@
 import SwiftUI
 
 struct HomeView: View {
+    
     @EnvironmentObject private var vm: HomeViewModel
-
-    @State private var showPortfolio: Bool = false
-    @State private var showPortfolioView: Bool = false
+    @State private var showPortfolio: Bool = false // animate right
+    @State private var showPortfolioView: Bool = false // new sheet
+    @State private var showSettingsView: Bool = false // new sheet
     @State private var selectedCoin: CoinModel? = nil
     @State private var showDetailView: Bool = false
-    @State private var showSettingsView: Bool = false
     
     var body: some View {
         ZStack {
+            // background layer
             Color.theme.background
                 .ignoresSafeArea()
                 .sheet(isPresented: $showPortfolioView, content: {
                     PortfolioView()
                         .environmentObject(vm)
                 })
-                .sheet(isPresented: $showSettingsView, content: {
-                    SettingsView()
-                })
             
+            // content layer
             VStack {
                 homeHeader
-                 
                 HomeStatsView(showPortfolio: $showPortfolio)
-                 
                 SearchBarView(searchText: $vm.searchText)
-                 
                 columnTitles
-             
+                
                 if !showPortfolio {
                     allCoinsList
                         .transition(.move(edge: .leading))
                 }
-                 
+                
                 if showPortfolio {
-                    portfolioCoinsList
-                        .transition(.move(edge: .trailing))
+                    ZStack(alignment: .top) {
+                        if vm.portfolioCoins.isEmpty && vm.searchText.isEmpty {
+                            portfolioEmptyText
+                        } else {
+                            portfolioCoinsList
+                        }
+                    }
+                    .transition(.move(edge: .trailing))
                 }
-             
+                
                 Spacer(minLength: 0)
             }
+            .sheet(isPresented: $showSettingsView, content: {
+                SettingsView()
+            })
         }
         .background(
             NavigationLink(
                 destination: DetailLoadingView(coin: $selectedCoin),
                 isActive: $showDetailView,
-                label: { EmptyView() }
-            )
+                label: { EmptyView() })
         )
     }
 }
 
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .navigationBarHidden(true)
+        }
+        .environmentObject(dev.homeVM)
+    }
+}
+
 extension HomeView {
+    
     private var homeHeader: some View {
         HStack {
             CircleButtonView(iconName: showPortfolio ? "plus" : "info")
@@ -68,18 +83,13 @@ extension HomeView {
                 .background(
                     CircleButtonAnimationView(animate: $showPortfolio)
                 )
-                .animation(nil, value: showPortfolio)
-             
             Spacer()
-
             Text(showPortfolio ? "Portfolio" : "Live Prices")
                 .font(.headline)
                 .fontWeight(.heavy)
                 .foregroundColor(Color.theme.accent)
-                .animation(nil, value: showPortfolio)
-             
+                .animation(.none)
             Spacer()
-
             CircleButtonView(iconName: "chevron.right")
                 .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
                 .onTapGesture {
@@ -89,6 +99,48 @@ extension HomeView {
                 }
         }
         .padding(.horizontal)
+    }
+        
+    private var allCoinsList: some View {
+        List {
+            ForEach(vm.allCoins) { coin in
+                CoinRowView(coin: coin, showHoldingsColumn: false)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+                    .onTapGesture {
+                        segue(coin: coin)
+                    }
+                    .listRowBackground(Color.theme.background)
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+    
+    private var portfolioCoinsList: some View {
+        List {
+            ForEach(vm.portfolioCoins) { coin in
+                CoinRowView(coin: coin, showHoldingsColumn: true)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+                    .onTapGesture {
+                        segue(coin: coin)
+                    }
+                    .listRowBackground(Color.theme.background)
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+    
+    private var portfolioEmptyText: some View {
+        Text("You haven't added any coins to your portfolio yet. Click the + button to get started! 🧐")
+            .font(.callout)
+            .foregroundColor(Color.theme.accent)
+            .fontWeight(.medium)
+            .multilineTextAlignment(.center)
+            .padding(50)
+    }
+    
+    private func segue(coin: CoinModel) {
+        selectedCoin = coin
+        showDetailView.toggle()
     }
 
     private var columnTitles: some View {
@@ -101,12 +153,11 @@ extension HomeView {
             }
             .onTapGesture {
                 withAnimation(.default) {
-                    vm.sortOption = (vm.sortOption == .rank) ? .rankReversed : .rank
+                    vm.sortOption = vm.sortOption == .rank ? .rankReversed : .rank
                 }
             }
-             
+            
             Spacer()
-             
             if showPortfolio {
                 HStack(spacing: 4) {
                     Text("Holdings")
@@ -116,24 +167,23 @@ extension HomeView {
                 }
                 .onTapGesture {
                     withAnimation(.default) {
-                        vm.sortOption = (vm.sortOption == .holdings) ? .holdingsReversed : .holdings
+                        vm.sortOption = vm.sortOption == .holdings ? .holdingsReversed : .holdings
                     }
                 }
             }
-             
             HStack(spacing: 4) {
+                Text("Price")
                 Image(systemName: "chevron.down")
                     .opacity((vm.sortOption == .price || vm.sortOption == .priceReversed) ? 1.0 : 0.0)
                     .rotationEffect(Angle(degrees: vm.sortOption == .price ? 0 : 180))
-                Text("Price")
-            }
-            .onTapGesture {
-                withAnimation(.default) {
-                    vm.sortOption = (vm.sortOption == .price) ? .priceReversed : .price
-                }
             }
             .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
-             
+            .onTapGesture {
+                withAnimation(.default) {
+                    vm.sortOption = vm.sortOption == .price ? .priceReversed : .price
+                }
+            }
+            
             Button(action: {
                 withAnimation(.linear(duration: 2.0)) {
                     vm.reloadData()
@@ -147,45 +197,5 @@ extension HomeView {
         .foregroundColor(Color.theme.secondaryText)
         .padding(.horizontal)
     }
-
-    private var allCoinsList: some View {
-        List {
-            ForEach(vm.allCoins) { coin in
-                CoinRowView(coin: coin, showHoldingsColumn: false)
-                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
-                    .onTapGesture {
-                        segue(coin: coin)
-                    }
-            }
-        }
-        .listStyle(PlainListStyle())
-    }
-
-    private var portfolioCoinsList: some View {
-        List {
-            ForEach(vm.portfolioCoins) { coin in
-                CoinRowView(coin: coin, showHoldingsColumn: true)
-                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
-                    .onTapGesture {
-                        segue(coin: coin)
-                    }
-            }
-        }
-        .listStyle(PlainListStyle())
-    }
     
-    private func segue(coin: CoinModel) {
-        selectedCoin = coin
-        showDetailView.toggle()
-    }
-}
-
-struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        NavigationView {
-            HomeView()
-                .navigationBarHidden(true)
-        }
-        .environmentObject(dev.homeVM)
-    }
 }
