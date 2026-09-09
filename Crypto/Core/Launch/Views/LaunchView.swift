64c729c04112f877de2b3783f6c9056bba4c58a1
berivan dağcı <100922483+berivan0108@users.noreply.github.com>
2026-09-09 18:27:11 +0300

    Add LaunchView, fix asset colors, and finalize project structure

---
+++ Crypto/Core/Launch/Views/LaunchView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchView.swift
+//  Crypto
+//
+//  Created by beri on 9.09.2026.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchView()
+}
